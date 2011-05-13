d90f94a441600e7a5bcf56e88c8bde4f204bafe4
David Brough <drbrough@utas.edu.au>
2011-05-14 08:57:57 +1000

    second try adding files in subfolder

---
+++ UnityScripts/Fabricator.js
@@ -0,0 +1,51 @@
+/*
+ * Fabricator.js
+ *
+ * David Brough
+ * OCDevs
+ * 11/05/2011
+ *
+ * A place to store the prefabs for all of the level and return then upon request
+ */
+ 
+//required prefabs for room construction
+var ceiling : GameObject;
+var door : GameObject;
+var dull : GameObject;
+var floor : GameObject;
+var roomLight : GameObject;
+var wall : GameObject;
+
+function getItem(ident)
+{
+	var handle : GameObject;
+	
+	switch(ident)
+	{
+		case "ceiling":
+			handle = ceiling;
+			break;
+		
+		case "door":
+			handle = door;
+			break;
+			
+		case "floor":
+			handle = floor;
+			break;
+		
+		case "roomLight":
+			handle = roomLight;
+			break;
+			
+		case "wall":
+			handle = wall;
+			break;
+
+		default:
+			handle = dull;
+			break;
+	}
+	
+	return handle;
+}
